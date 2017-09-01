5866248cded8a1421e9ff35d99dda92a5bf733e2
Syed Anab Imam <syedanabimam@gmail.com>
2017-09-01 16:53:02 +0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Agent.create([ { name: 'john', code: 'JO-12312', skills: ['fighting','running'] }, { name: 'smith', code: 'SM-12312', skills: ['fighting','shooting'] } ])
+
+Sentinel.create([ { name: 'rojarous', code: 'RO-12312', group: 'A', skills: ['hacking','injecting'] }, { name: 'lezarus', code: 'SM-12312', group: 'A', skills: ['nicking','shooting'] } ])
